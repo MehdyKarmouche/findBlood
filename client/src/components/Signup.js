13bd337f75e5da5c6c83e33d9d84d67ce99f8926
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-09-20 11:25:44 +0000

    Signup for centers
    Material UI components

---
+++ client/src/components/Signup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Signupform from './Signupform';
+
+function Home() {
+  return (
+    <div className="Home">
+        <Navbar/>
+        <Signupform/>
+        
+    </div>
+  );
+}
+
+export default Home;
