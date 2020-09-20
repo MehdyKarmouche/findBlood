1475783a3b8c657bd9f1abf1cf10ab5608703ad8
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-09-20 14:39:12 +0000

    Sign in for Centers

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
-import Signin from './components/donor/Signin';
+import Signin from './components/center/Signin';
 
 function App() {
   return (
