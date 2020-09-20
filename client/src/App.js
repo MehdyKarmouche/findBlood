40e560db3c406e88d68c7a8a9efdbac54b0daab1
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-09-20 11:30:49 +0000

    Refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
-import Signup from './components/Signup';
+import Signup from './components/center/Signup';
 
 function App() {
   return (
