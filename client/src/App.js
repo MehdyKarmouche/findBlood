27f495be1a28dfcc875512b5d11cc77bed6a5035
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-09-20 11:39:09 +0000

    Signup for Donors

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
-import Signup from './components/center/Signup';
+import Signup from './components/donor/Signup';
 
 function App() {
   return (
