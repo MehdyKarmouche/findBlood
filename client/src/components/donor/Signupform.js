03a4ed255c1047f74cae3852e7a1400569acdbd6
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-10-17 13:37:13 +0000

    Refactor

--- client/src/components/donor/Signupform.js
+++ client/src/components/donor/Signupform.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, }  from 'react';
+import React, { useState }  from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
