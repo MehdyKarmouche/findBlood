c42992baf8f8a3a3be600795bf9a4e994c5be4ee
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-11-16 18:40:17 +0000

    Fix: go from signup to signin of the other role

--- client/src/components/center/pages/Signup.js
+++ client/src/components/center/pages/Signup.js
@@ -3,6 +3,7 @@ import Navbar from '../../Navbar';
 import Signupform from '../Signupform';
 import Signin from './Signin';
 import Home from '../../Home'
+import DonorSignin from '../../donor/pages/Signin'
 import {BrowserRouter as Router, Route} from  'react-router-dom'
 
 const Signup = () => {
@@ -17,6 +18,7 @@ const Signup = () => {
 
 } />
     <Route exact path="/center/signin" component={Signin}/>
+    <Route exact path="/donor/signin" component={DonorSignin}/>
     <Route exact path="/" component={Home}/>
     </div>
     </Router>
--- client/src/components/donor/pages/Signup.js
+++ client/src/components/donor/pages/Signup.js
@@ -4,6 +4,7 @@ import Signupform from '../Signupform';
 import Signin from './Signin';
 import Home from '../../Home'
 import Footer from '../../Footer'
+import CenterSignin from '../../center/pages/Signin'
 import {BrowserRouter as Router, Route} from  'react-router-dom'
 
 const Signup = () => {
@@ -19,6 +20,7 @@ const Signup = () => {
 
 } />
     <Route exact path="/donor/signin" component={Signin}/>
+    <Route exact path="/center/signin" component={CenterSignin}/>
     <Route exact path="/" component={Home}/>
     </div>
     </Router>
